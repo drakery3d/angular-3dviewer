2de9b88d59f3a26c7da07942f1a706d7cacab0af
flolu <flo@flolu.com>
2020-07-17 12:48:23 +0200

    hemisphere

--- viewer/src/app/viewer.component.ts
+++ viewer/src/app/viewer.component.ts
@@ -10,14 +10,14 @@ import {
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { HorizontalBlurShader } from 'three/examples/jsm/shaders/HorizontalBlurShader';
-import { VerticalBlurShader } from 'three/examples/jsm/shaders/VerticalBlurShader';
-import { EffectPass } from 'postprocessing';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass';
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+// import { HorizontalBlurShader } from 'three/examples/jsm/shaders/HorizontalBlurShader';
+// import { VerticalBlurShader } from 'three/examples/jsm/shaders/VerticalBlurShader';
+// import { EffectPass } from 'postprocessing';
+// import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass';
 
 @Component({
   selector: 'app-viewer',
@@ -48,14 +48,16 @@ export class ViewerComponent implements AfterViewInit, OnDestroy {
 
   private frameId: number = null;
 
-  private cube1: THREE.Mesh;
-  private cube2: THREE.Mesh;
-
   constructor(private ngZone: NgZone) {}
 
   ngAfterViewInit() {
     this.createScene(this.renderCanvas);
-    this.animate();
+    this.createTestScene();
+
+    this.ngZone.runOutsideAngular(() => {
+      this.render();
+      window.addEventListener('resize', () => this.resize());
+    });
   }
 
   ngOnDestroy(): void {
@@ -67,16 +69,22 @@ export class ViewerComponent implements AfterViewInit, OnDestroy {
   createScene(canvas: ElementRef<HTMLCanvasElement>): void {
     this.canvas = canvas.nativeElement;
 
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: this.canvas,
-      antialias: true,
-    });
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.autoUpdate = false;
+    this.renderer.toneMapping = THREE.ReinhardToneMapping;
+    this.renderer.toneMappingExposure = 2.2;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setClearColor(0xffffff, 1);
-    this.renderer.autoClear = false; // NOW what?
 
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xffffff);
+
+    const hemiLight = new THREE.HemisphereLight();
+    hemiLight.intensity = 3;
+    hemiLight.castShadow = true;
+    this.scene.add(hemiLight);
 
     this.camera = new THREE.PerspectiveCamera(
       75,
@@ -84,12 +92,9 @@ export class ViewerComponent implements AfterViewInit, OnDestroy {
       0.1,
       1000
     );
-    this.camera.position.z = 20;
-    this.camera.position.y = 10;
-    this.scene.add(this.camera);
+    this.camera.position.set(0, 3, 3);
 
-    this.scene.add(new THREE.DirectionalLight());
-    this.scene.add(new THREE.HemisphereLight());
+    this.scene.add(this.camera);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.rotateSpeed = 1.5;
@@ -98,17 +103,7 @@ export class ViewerComponent implements AfterViewInit, OnDestroy {
     this.composer = new EffectComposer(this.renderer);
     var renderPass = new RenderPass(this.scene, this.camera);
     this.composer.addPass(renderPass);
-    /* const ssaoPass = new SSAOPass(
-      this.scene,
-      this.camera,
-      window.innerWidth,
-      window.innerHeight
-    );
-    ssaoPass.kernelRadius = 16;
-    ssaoPass.minDistance = 0.005;
-    ssaoPass.maxDistance = 0.02;
-    ssaoPass.output = 0;
-    this.composer.addPass(ssaoPass); */
+
     this.fxaaPass = new ShaderPass(FXAAShader);
     var pixelRatio = this.renderer.getPixelRatio();
     this.fxaaPass.material.uniforms['resolution'].value.x =
@@ -116,51 +111,47 @@ export class ViewerComponent implements AfterViewInit, OnDestroy {
     this.fxaaPass.material.uniforms['resolution'].value.y =
       1 / (this.canvas.offsetHeight * pixelRatio);
     this.composer.addPass(this.fxaaPass);
-
-    this.createTestScene();
   }
 
   private createTestScene() {
-    this.cube1 = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(10, 10, 10),
-      new THREE.MeshBasicMaterial({ color: 0xeeee99 })
-    );
-    this.cube1.position.x = -5;
-    this.scene.add(this.cube1);
+    const planeGeometry = new THREE.PlaneGeometry(10, 10);
+    planeGeometry.rotateX(-Math.PI / 2);
+    const planeMaterial = new THREE.ShadowMaterial({ color: 0xdddddd });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+    this.scene.add(plane);
 
-    this.cube2 = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(10, 10, 10),
-      new THREE.MeshBasicMaterial({ color: 0x99eeee })
-    );
-    this.cube2.position.x = 5;
-    this.cube2.position.y = 3;
-    this.scene.add(this.cube2);
-  }
-
-  animate() {
-    this.ngZone.runOutsideAngular(() => {
-      if (document.readyState !== 'loading') {
-        this.render();
-      } else {
-        window.addEventListener('DOMContentLoaded', () => {
-          this.render();
-        });
-      }
-
-      window.addEventListener('resize', () => {
-        this.resize();
-      });
+    const boxGeometry = new THREE.BoxGeometry();
+    const boxMaterial = new THREE.MeshPhysicalMaterial({
+      color: 0x11687a,
     });
+    const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
+    this.scene.add(cube);
+    /* new GLTFLoader().load('assets/Astronaut.glb', (result) => {
+      const model = result.scene.children[0];
+      model.scale.set(10, 10, 10);
+      model.traverse((obj) => {
+        if ((obj as any).isMesh) {
+          obj.castShadow = true;
+          obj.receiveShadow = true;
+          if ((obj as any).material.map) {
+            (obj as any).material.map.anisotropy = 16;
+          }
+        }
+      });
+
+      var bb = new THREE.Box3();
+      bb.setFromObject(model);
+      bb.getCenter(this.controls.target);
+
+      this.scene.add(model);
+    }); */
   }
 
   render(): void {
-    this.frameId = requestAnimationFrame(() => {
-      this.render();
-    });
-
-    this.cube1.rotation.y += 0.01;
-    this.cube2.rotation.y -= 0.01;
-
+    this.frameId = requestAnimationFrame(() => this.render());
     this.composer.render();
   }
 
