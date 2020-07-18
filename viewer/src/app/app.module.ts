19b72a44616d9cc432fd17b7ab57de1c9e393a72
flolu <flo@flolu.com>
2020-07-18 08:28:05 +0200

    fullscreen toggler

--- viewer/src/app/app.module.ts
+++ viewer/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ViewerComponent } from './viewer.component';
+import {AppComponent} from './app.component';
+import {ViewerComponent} from './viewer.component';
+import {FullscreenService} from './fullscreen.service';
 
 @NgModule({
   declarations: [AppComponent, ViewerComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [FullscreenService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
